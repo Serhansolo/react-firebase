3e2699b7094e2b5c0556de480513dbaf7a29a8ff
Serhan Uygur <serhan.uygur@gmail.com>
2020-02-20 14:24:22 +0100

    test trigger

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload?
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          test
+          </p>
       </header>
     </div >
   );
